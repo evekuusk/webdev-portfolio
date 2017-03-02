dc566e173cbe3ba804bccd42e7cbd3d6d6e355d7
Eve Kuusk <eve.kuusk.dev@gmail.com>
2017-03-01 21:40:53 -0500

    significant progress on layout and design

--- src/js/components/layout/footer.js
+++ src/js/components/layout/footer.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import Scroll from 'react-scroll';
+
+// react-scroll library variables
+var Link       = Scroll.Link;
+var Element    = Scroll.Element;
+var Events     = Scroll.Events;
+var scroll     = Scroll.animateScroll;
+var scrollSpy  = Scroll.scrollSpy;
 
 export default class Footer extends Component {
   constructor(props) {
     super(props);
   };
+  backToTop() {
+    scroll.scrollToTop({
+      duration: 350
+    });
+  }
   render() {
     return (
       <div className="footer">
-        footer
+        <button className="back-to-top" onClick={() => this.backToTop()}>^</button>
       </div>
     )
   }
--- src/js/components/sections/home/home-content.js
+++ src/js/components/sections/home/home-content.js
@@ -5,23 +5,62 @@ export default class HomeContent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      'intro': []
+      'intro': [],
+      'learning': [],
+      'education': []
     }
   }
   componentDidMount() {
-    console.log(introData)
     this.setState({
-      'intro': introData['ABOUT_PARAGRAPH']
+      'intro': introData['ABOUT_PARAGRAPH'],
+      'learning': introData['INDEPENDENT_LEARNING'],
+      'education': introData['EDUCATION']
     })
   }
   render() {
     const introParagraph = this.state.intro.map(function(paragraph, index) {
       return <p key={index}>{paragraph}</p>
+    });
+
+    const learningList = this.state.learning.map(function(item, index) {
+      var items = []
+      for (var i = 0; i < item.length; i++) {
+        items.push(item[i])
+      }
+      items.shift()
+      items.shift()
+      items = items.join(', ')
+      return (
+        <p key={index} className="learning-list">
+          <a href={item[1]}><span className="learning-item">{item[0]}</span></a>:
+          <span className="list">{items}</span>
+        </p>
+      )
+    });
+    const educationList = this.state.education.map(function(item, index) {
+      var items = []
+      for (var i = 0; i < item.length; i++) {
+        items.push(item[i])
+      }
+      items.shift()
+      items = items.join(', ')
+      return (
+        <p key={index} className="education-list">
+          <span className="education-item">{item[0]}</span>
+          <br />
+          <span className="list">{items}</span>
+        </p>
+      )
     })
     return (
       <div className="home-content content">
-        <h4>Introduction</h4>
-        {introParagraph}
+        <h3>Introduction</h3>
+          {introParagraph}
+        <h3>Self-Directed Learning</h3>
+          {learningList}
+        <h3>Education</h3>
+          <p>Currently taking pre-requisite math courses for admission into part-time evening classes for computer science</p>
+          {educationList}
       </div>
     )
   };
