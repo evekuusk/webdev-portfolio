00acfb7c7e11096ea7e394428472f5919f6cbaff
Eve Kuusk <eve.kuusk.dev@gmail.com>
2017-03-02 12:02:44 -0500

    progress on contact form

--- src/js/components/sections/contact/contact-content.js
+++ src/js/components/sections/contact/contact-content.js
@@ -8,7 +8,12 @@ export default class ContactContent extends Component {
       'submit_enabled': false,
       'submit_email': '',
       'submit_name': '',
-      'submit_message': ''
+      'submit_message': '',
+      'submit_company': '',
+      'company_validated': false,
+      'email_validated': false,
+      'name_validated': false,
+      'message_validated': false
     }
   }
   onloadCallback() {
@@ -20,19 +25,58 @@ export default class ContactContent extends Component {
       'submit_enabled': true
     })
   }
+  checkSubmitEnabled() {
+    if (this.state.email_validated === true && this.state.name_validated === true && this.message_validated === true && this.company_validated === true) {
+      this.setState({
+        'submit_enabled': true
+      })
+    }
+  }
+  handleNameInput(event) {
+    var value = event.target.value
+    this.setState({
+      'submit_name': value
+    })
+    this.checkSubmitEnabled()
+  }
+  handleCompanyInput(event) {
+    var value = event.target.value
+    this.setState({
+      'submit_company': value
+    })
+    this.checkSubmitEnabled()
+  }
+  handleEmailInput(event) {
+    var value = event.target.value
+    this.setState({
+      'submit_email': value
+    })
+    // validate email...
+    // set email validated...
+    this.checkSubmitEnabled()
+  }
+  handleMessageInput(event) {
+    var value = event.target.value
+    this.setState({
+      'submit_message': value
+    })
+    this.checkSubmitEnabled()
+  }
   submitForm() {
-
+    console.log('Email sent')
   }
   render() {
     return (
       <div className="contact-content content">
         <form>
           <h5 className="input-label">Name:</h5>
-            <input type="text"></input>
+            <input type="text" name="Name Input" onChange={this.handleNameInput.bind(this)} value={this.state.submit_name}></input>
+          <h5 className="input-label">Company:</h5>
+            <input type="text" name="Company Input" onChange={this.handleCompanyInput.bind(this)} value={this.state.submit_company}></input>
           <h5 className="input-label">Email:</h5>
-            <input type="text"></input>
+            <input type="text" name="Email Input" onChange={this.handleEmailInput.bind(this)} value={this.state.submit_email}></input>
           <h5 className="input-label">Message:</h5>
-          <textarea maxLength="400"></textarea>
+          <textarea name="Message Input" maxLength="400" onChange={this.handleMessageInput.bind(this)} value={this.state.submit_message}></textarea>
             <br />
 
           <Recaptcha sitekey="6LfqchcUAAAAAJi5QCHhVBQt49a0tDtpfyTDj_6h" render="explicit" onloadCallback={this.onloadCallback} verifyCallback={this.verifyCallback.bind(this)} />
@@ -42,6 +86,3 @@ export default class ContactContent extends Component {
     )
   };
 };
-
-// <Recaptcha sitekey="6LfqchcUAAAAAJi5QCHhVBQt49a0tDtpfyTDj_6h" />
-// <div className="g-recaptcha" data-sitekey="6LfqchcUAAAAAJi5QCHhVBQt49a0tDtpfyTDj_6h"></div>
